42d44021216c63e5bffe33e499261f5543baf0df
nnaka2992 <nnaka2992@gmail.com>
2022-07-23 03:38:42 +0900

    Implement addition and subtraction

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,14 +12,42 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Invalid number of arguments.\n")
 		os.Exit(1)
 	}
-	i, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		os.Exit(2)
-	}
-
+	expr := os.Args[1]
 	fmt.Printf(".intel_syntax noprefix\n")
 	fmt.Printf(".globl main\n")
 	fmt.Printf("main:\n")
-	fmt.Printf("  mov rax, %d\n", i)
+	v, i := StrToInt(expr)
+	fmt.Printf("  mov rax, %d\n", v)
+	for i < len(expr) {
+		switch expr[i] {
+		case '+':
+			i++
+			v, offset := StrToInt(expr[i:])
+			i += offset
+			fmt.Printf("  add rax, %d\n", v)
+			continue
+		case '-':
+			i++
+			v, offset := StrToInt(expr[i:])
+			i += offset
+			fmt.Printf("  sub rax, %d\n", v)
+			continue
+		default:
+			fmt.Fprintf(os.Stderr, "Unexpected character %s.\n", string(expr[i]))
+			os.Exit(1)
+		}
+	}
 	fmt.Printf("  ret\n")
+}
+
+func StrToInt(s string) (v int, offset int) {
+	offset = strings.IndexFunc(s, func(r rune) bool { return r < '0' || r > '9' })
+	if offset == -1 {
+		offset = len(s)
+	}
+	if offset == 0 {
+		return
+	} // Avoid Atoi on empty string
+	v, _ = strconv.Atoi(s[:offset])
+	return
 }
